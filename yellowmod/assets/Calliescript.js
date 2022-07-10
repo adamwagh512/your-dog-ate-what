cca3f1c0ec505aad5c596a1367b59f3ef3631c78
Callie Williams <calliebwill@gmail.com>
2022-07-10 09:13:11 -0500

    Psuedo code for and foundation for Maps API implimitation.

---
+++ yellowmod/assets/Calliescript.js
@@ -0,0 +1,19 @@
+function initMap() {
+    
+    var options = {
+        center: {lat: 30.2672 lng:-97.7431 };
+        zoom: 8
+    }
+
+    map = new google.maps.Map(document.getElementById("map"), options)
+}
+
+// Set up function for searching maps for "vet", "veterinarian", "animal hospital" , "24 hours animal hospital"
+// (Source:  https://developers.google.com/maps/documentation/javascript/places )
+// Add Places API
+
+// Create loop for search.
+
+// Google Geolocation API Here
+
+// Create function that allows these to talk to each other. 
