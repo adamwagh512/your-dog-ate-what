4cfdc35d19040cc7a328436c8eccf67d6732d495
Sierra Nutt <sierra.nutt@outlook.com>
2022-07-11 09:40:05 -0600

    New Edits SN

--- assets/js/food_api.js
+++ assets/js/food_api.js
@@ -1,17 +1,22 @@
 
 var access_key = '6orO8TB9RRot89tK';
-var userInput = "bread";
+var userInput = "Cheese";
+
+//generateBtn.addEventListener("click", writeItem);
 
         $.ajax({ 
             type: 'GET',
-            url: 'https://chompthis.com/api/v2/food/branded/name.php?api_key=6orO8TB9RRot89tK&name=egg',
+            url: 'https://chompthis.com/api/v2/food/branded/name.php?api_key=6orO8TB9RRot89tK&name=' + userInput,
             dataType: 'json',
             success: function(data) {
-
-                for (i=0; i< 10; i++){
-                console.log(data.items[i].name);
-                console.log(data.items[i].ingredients);
-             }
-            }
+                
+                for (i=0; i< data.items.length; i++){
+                    console.log(data.items[i])
+                    console.log(data.items[i].ingredients)
+                }
+            }         
         });
+
+
+
 
