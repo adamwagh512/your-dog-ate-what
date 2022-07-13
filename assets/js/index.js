99b862cbc55d58978cc59deb373b5b42dd020e61
Kurt Bixby <kurtbixby@mac.com>
2022-07-12 23:39:48 -0500

    Recent searches work

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,32 +9,49 @@ const MAYBE_HAZARD_ID = 'maybe-hazard-modal';
 const SEVERE_HAZARD_ID = 'definite-hazard-modal';
 const UNKNOWN_HAZARD_ID = 'unknown-hazard-modal';
 
+function init() {
+  $('.rs').on('click', onclickhandler);
+  loadRecentSearches();
+}
+
 //This function loads recent searches from local storage when page is loaded
 function loadRecentSearches() {
+  let retrievedSearchesString = localStorage.getItem("recentSearches");
+  if (retrievedSearchesString == null) {
+    return;
+  }
+
   // parses the stringified data back into an array
-  var storedSearches = JSON.parse(localStorage.getItem("recentSearches"));
+  let storedSearches = JSON.parse(retrievedSearchesString);
+  let filteredSearches = storedSearches.filter(search => search.trim().length)
+  recentSearches = filteredSearches;
+  
   // Console log for testing purposes
-  console.log(storedSearches);
+  console.log(filteredSearches);
   // For each item in the array,
-  for (var i = 0; i < storedSearches.length; i++) {
+  for (var i = 0; i < filteredSearches.length; i++) {
     // A new button is revealed
     var button = $(`#rs${i + 1}`);
     button.show();
     // The button displays the search term
-    button.text(storedSearches[i]);
+    button.text(filteredSearches[i]);
     //on click calls a function defined below, figured we would need it later
-    button.on("click", onclickhandler);
+    // button.on("click", onclickhandler);
   }
 }
 
 //this function is called every time the submit button is clicked.
 function searchFunction(userInput, recentSearch = false) {
+  if (!userInput) {
+    return;
+  }
+
   if (!recentSearch) {
     console.log(userInput);
     // Check if empty
   
       // pushes eaten value into an empty array
-      recentSearches.unshift($("#eaten").val());
+      recentSearches.unshift(userInput);
   
       // If there are more than 4 recent searches, the oldest one is removed from the array
       if (recentSearches.length > 4) {
@@ -86,10 +103,8 @@ function searchFunction(userInput, recentSearch = false) {
 //This function was made to try to make it easier to wire everything up later
 function onclickhandler(event) {
   // console log for testing purposes
-  console.log(event.target);
   // Call with the string
   let searchTerm = $(event.target).text();
-  console.log(searchTerm);
   searchFunction(searchTerm, true);
 }
 // Once the submit button is clicked, take the input from textbox and save it to local storage
@@ -167,9 +182,7 @@ $("#buttonsContainer").children().hide();
 
 // Once the submit button is clicked, take the input from textbox and save it to local storage. The functions on the end will help populate the recent searches bar and clear the text from the search bar.
 $("#submitBtn").on("click", function () {
-  var typed = $("#eaten").val();
-  localStorage.setItem("searchValue", typed);
-  // Console log for testing purposes
+  var typed = $("#eaten").val().trim();
   console.log(typed);
   // calls searchFunction from above
   searchFunction(typed);
@@ -178,8 +191,8 @@ $("#submitBtn").on("click", function () {
   // refreshes load receent searches function
   loadRecentSearches();
 });
+init();
 //loads recent searches from local storage when page is loaded
-loadRecentSearches();
 
 function myMap() {
   var mapProp = {
