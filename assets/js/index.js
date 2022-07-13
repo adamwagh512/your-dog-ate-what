412e8d08d5e6dff3ae43c721e2486a283f5a0e46
Kurt Bixby <kurtbixby@mac.com>
2022-07-13 05:23:54 -0500

    New NavBar is working

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,6 +29,14 @@ function init() {
     $(this).parents('.modal').removeClass("is-active");
   });
 
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+  });
+
   initModals();
 
   $('.'+RECENT_BUTTON_CLASS).on('click', recentSearchButtonHandler);
